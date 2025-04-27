bfded175d14c535c862fba02433d932186558d60
marcellusmbarbosa <marcellusmbarbosa@gmail.com>
2025-04-27 06:57:03 -0300

    Atualizando o repositorio

---
+++ Semana04/Exercicio02/Video03/01Conceitos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01Conceitos
+{
+    class Porgram
+    {
+        static void Main(string[] args)
+        {
+            
+            Person p1 = new Person();
+            p1.name = "Marco Tulio";
+            p1.age = 22;
+
+            p1.message();
+            
+        }
+    }
+}
