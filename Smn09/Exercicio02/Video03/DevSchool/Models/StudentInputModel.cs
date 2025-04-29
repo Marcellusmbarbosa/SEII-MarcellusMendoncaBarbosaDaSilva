3cf17d618dec7b479edcdec7d63ad70e97bd48c6
marcellusmbarbosa <marcellusmbarbosa@gmail.com>
2025-04-28 23:16:43 -0300

    Clean

--- Smn09/Exercicio02/Video03/DevSchool/Models/StudentInputModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DevSchool.Models
-{
-    public class StudentInputModel
-    {
-        public string FullName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string SchoolClass { get; set; }
-    }
-}
--- Smn09/Exercicio02/Video03/DevSchoolDbCreate.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Students (
-	Id INT IDENTITY(1,1) PRIMARY KEY,
-	FullName VARCHAR(100),
-	BirthDate DATETIME,
-	SchoolClass VARCHAR(50),
-	IsActive BIT
-)
-
-DROP TABLE Students;
